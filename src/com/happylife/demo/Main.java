4db97da1499a7131ef223ba28a9d757c92c12144
liguanghao <liguanghao@viatomtech.com>
2021-11-23 13:23:23 +0800

    Initial commit

---
+++ src/com/happylife/demo/Main.java
@@ -0,0 +1,18 @@
+package com.happylife.demo;
+
+public class Main {
+	public static int port = 9000;
+	public static void main(String[] args) {
+//		// start http server
+//		SimpleHttpServer httpServer = new SimpleHttpServer();
+//		httpServer.Start(port);
+		
+		// start https server
+		SimpleHttpServer httpsServer = new SimpleHttpServer();
+		httpsServer.Start(port);
+		
+//		System.out.println(System.getProperty("user.dir"));
+//		System.out.println(Main.class.getClassLoader().getResource("").getPath());
+		
+	}
+}
