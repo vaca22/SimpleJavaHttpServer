11a9fb7a4c84e445cbb1ce94c06dd397a205d42d
vaca22 <wsmslgh@gmail.com>
2022-05-30 20:57:17 +0800

    fdgedrfsafdg

--- src/com/happylife/demo/Handlers.java
+++ src/com/happylife/demo/Handlers.java
@@ -35,9 +35,21 @@ public class Handlers {
 	public static class EchoPostHandler implements HttpHandler {
 		@Override
 		public void handle(HttpExchange he) throws IOException {
-			System.out.println("Served by /echoPost handler...");
-			// parse request
-			Map<String, Object> parameters = new HashMap<String, Object>();
+
+			//--------------url thing
+			Map<String, String> parameters = new HashMap<String, String>();
+			URI requestedUri = he.getRequestURI();
+			String query = requestedUri.getRawQuery();
+			parseQuery(query, parameters);
+			String fuckx=parameters.get("filename");
+			System.out.println(parameters);
+
+
+
+			String fuckaaa= he.getRequestHeaders().get("filename").get(0);
+			System.out.println(fuckaaa);
+
+
 			InputStreamReader isr = new InputStreamReader(he.getRequestBody(), "utf-8");
 			BufferedReader br = new BufferedReader(isr);
 			char[] fuck=new char[1000];
@@ -55,12 +67,9 @@ public class Handlers {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
-	public static void parseQuery(String query, Map<String, Object> parameters) throws UnsupportedEncodingException {
-
+	public static void parseQuery(String query, Map<String, String> parameters) throws UnsupportedEncodingException {
 		if (query != null) {
 			String pairs[] = query.split("[&]");
-
 			for (String pair : pairs) {
 				String param[] = pair.split("[=]");
 
@@ -69,25 +78,10 @@ public class Handlers {
 				if (param.length > 0) {
 					key = URLDecoder.decode(param[0], System.getProperty("file.encoding"));
 				}
-
 				if (param.length > 1) {
 					value = URLDecoder.decode(param[1], System.getProperty("file.encoding"));
 				}
-
-				if (parameters.containsKey(key)) {
-					Object obj = parameters.get(key);
-					if (obj instanceof List<?>) {
-						List<String> values = (List<String>) obj;
-						values.add(value);
-					} else if (obj instanceof String) {
-						List<String> values = new ArrayList<String>();
-						values.add((String) obj);
-						values.add(value);
-						parameters.put(key, values);
-					}
-				} else {
-					parameters.put(key, value);
-				}
+				parameters.put(key, value);
 			}
 		}
 	}
